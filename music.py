be66131532d493a6fcfa49a5bd3ec76c177aa339
mcrrobinson <mrmcrrobinson@gmail.com>
2020-07-23 14:14:23 +0100

    [Added] - Main.

---
+++ music.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+import youtube_dl
+import os
+from sys import argv
+
+download_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+
+if not os.path.exists('Songs'):
+    os.mkdir('Songs')
+else:
+    os.chdir('Songs')
+    
+with youtube_dl.YoutubeDL(download_options) as dl:
+    with open('../' + argv[1], 'r') as f:
+        for song_url in f:
+            dl.download([song_url])
+
+
